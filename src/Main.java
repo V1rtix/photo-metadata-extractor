3bc61cc9032202ca238114999ac6565e4cbe0f5d
Virtix <viktorpawlas@gmail.com>
2025-08-08 08:21:07 +0200

    Now supports .ARW files + few changes to code

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,76 @@ import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
-import com.drew.metadata.exif.ExifSubIFDDescriptor;
-import com.drew.metadata.exif.ExifSubIFDDirectory;
-
+import static com.drew.metadata.exif.ExifSubIFDDirectory.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) throws ImageProcessingException, IOException {
+    public static void main(String[] args) {
 
-        String photosPath = "photos";
-        File photoFolder = new File(photosPath);
+        File photoFolder = new File("photos");
 
-        for (File file : photoFolder.listFiles()) {
-            if (file != null) {
+        try {
+            for (File file : Objects.requireNonNull(photoFolder.listFiles())) {
                 processPhoto(file);
-            } else {
-                System.out.println("No photos to process");
             }
+        } catch (ImageProcessingException | IOException e) {
+            System.out.println("No photos to process, insert photos to process");
         }
-
-        File photo = new File("photos/test.jpg/");
-
     }
 
     private static void processPhoto(File photo) throws ImageProcessingException, IOException {
         Metadata metadata = ImageMetadataReader.readMetadata(photo);
-        ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+        String focalLength = null;String fNumber = null;String iso = null;String exposureTime = null;
 
-        ExifSubIFDDescriptor descriptor = new ExifSubIFDDescriptor(directory);
+        for (Directory dir : metadata.getDirectories()) {
+            if (focalLength == null) focalLength = dir.getDescription(TAG_FOCAL_LENGTH);
+            if (fNumber == null) fNumber = dir.getDescription(TAG_FNUMBER);
+            if (iso == null) iso = dir.getDescription(TAG_ISO_EQUIVALENT);
+            if (exposureTime == null) exposureTime = dir.getDescription(TAG_EXPOSURE_TIME);
+        }
+        if (photo.getName().toLowerCase().endsWith(".arw") && exposureTime != null) exposureTime = transferExposureTime(exposureTime);
 
-        String name = descriptor.getFileSourceDescription();
-        String focalLength = descriptor.getFocalLengthDescription();
-        String fNumber = descriptor.getFNumberDescription();
-        String iso = descriptor.getIsoEquivalentDescription();
-        String exposure = descriptor.getExposureTimeDescription();
+        System.out.println(photo.getName() + " - " + focalLength + " | " + fNumber + " | ISO-" + iso + " | " + exposureTime);
+    }
+    // Method that transfers Exposure time from decimal to fractional version (0,02 sec -> 1/50 sec)
+    // Only for .ARW files because metadata in .JPEG are already in fractional version
+    private static String transferExposureTime(String exposureTime) {
+        double time = Double.parseDouble(exposureTime.replace(" sec", ""));
+        if (time >= 1){
+            return time + " sec";
+        }
+        int result = 1;
 
-        System.out.println(photo.getName() + " - " + focalLength + " | " + fNumber + " | ISO-" + iso + " | " + exposure);
+        while (time < 1) {
+            time = time * 10;
+            result = result * 10;
+        }
+        if (time == 1){
+            return "1/" + result + " sec";
+        }
+        if (time % 2 == 0) {
+            while (time != 1) {
+                time = time / 2;
+                result = result / 2;
+            }
+            return "1/" + result + " sec";
+        }
+        if (time % 3 == 0) {
+            while (time != 1) {
+                time = time / 3;
+                result = result / 3;
+            }
+            return "1/" + result + " sec";
+        }
+        if (time % 5 == 0){
+            while (time != 1) {
+                time = time / 5;
+                result = result / 5;
+            }
+            return "1/" + result + " sec";
+        }
+        return "Error occurred while transferring exposure time!";
     }
 }
